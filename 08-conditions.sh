ad958cb3b94d7e0dd6d1d4c408f280e33c7e79e5
Mahesh8120 <jonnalagaddamahi@gmail.com>
2023-12-31 09:46:03 +0530

    practice

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,7 +3,7 @@
  NUMBER=$1
  if [ $NUMBER -gt 100 ]
  then    
-   echo [ print given $NUMBER is greater than 100]
+   echo [ print given $NUMBER is greater than 100 ]
  else
-   echo [ print given $NUMBER is not greater than 100]
+   echo [ print given $NUMBER is not greater than 100 ]
  fi            

dbe2f9f604fd3948fdaed6a154f27851f90e38f3
Mahesh8120 <jonnalagaddamahi@gmail.com>
2023-12-31 23:22:12 +0530

    bug fix

---
+++ 09-functions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+ID=$(id -u)
+
+if ($ID -ne 0)
+then 
+    echo "error: run this script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+yum install git -y
+
+
+

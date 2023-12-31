982918e65070a302b1d8d1ba1356229b1f6e85b7
Mahesh8120 <jonnalagaddamahi@gmail.com>
2023-12-31 09:52:43 +0530

    practice

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
  
- id
+  ID=$(id -u)
  
+  if [$ID ne 0]
+  then
+      echo "error: please run this script with root access"
+  else 
+      echo "you are root user"
+  if   
+
+   yum install mysql -y
+           
+        

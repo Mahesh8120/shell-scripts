84b5a41d7077891f3e125d893c9c916a9ee342a6
Mahesh8120 <jonnalagaddamahi@gmail.com>
2024-01-01 21:50:04 +0530

    pratice

--- 10-mysql.sh
+++ 10-mysql.sh
@@ -5,19 +5,19 @@ ID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0  ]
     then
-        echo"Error: $2 is failed"
+        echo "Error: $2 is failed"
         exit 1 
     else
-        echo" $2 success"  
+        echo "$2 success"  
     fi 
 }    
     
      if [ $ID -ne 0 ]
     then
-        echo"Error:run this script with root access"
+        echo "Error:run this script with root access"
         exit 1
     else
-        echo"you are root user"
+        echo "you are root user"
     fi    
 
     yum install mysql -y

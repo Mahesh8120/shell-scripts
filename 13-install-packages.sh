0c14a66b7f9b1df16489f05fd863ec28cd6e1c32
Mahesh8120 <jonnalagaddamahi@gmail.com>
2024-01-28 17:13:03 +0530

    practice

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -5,12 +5,15 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+ echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE(){                            
   if [ $1 -ne 0 ]
   then
       echo -e "$2 .....$R failed $N"
-      exit 1
+      exit 1 
   else
       echo -e "$2 .....$G success $N"
   fi   
@@ -24,8 +27,14 @@ else
     echo -e "$G you are root user $N"
 fi    
 
-yum install git -y
-VALIDATE $? "installing git"
-
-yum install mysql -y
-VALIDATE $? "installing mysql"
+for package in $@
+do
+    yum list installed $package
+    if [ id -ne 0 ]
+    then 
+        echo -e "yum install $package -y"
+              VALIDATE $? "installing $package"
+    else
+        echo -e "$package is already installed..... $Y skipping $N"
+    fi       
+done
